48ec10a64f9b7787ca2f6836fcb57763ede26331
ezequiel reyes <110490540+EzeReyes@users.noreply.github.com>
2023-10-16 11:40:38 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+require('./config/db');
+
+const express = require('express');
+const exphbs = require('express-handlebars');
+const path = require('path');
+const router = require('./routes');
+const cookieParser = require('cookie-parser')
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+
+require('dotenv').config({path : 'variables.env'});
+
+const app = express();
+
+// Habilitar handlebars como view
+app.engine('handlebars',exphbs.engine({defaultLayout:'main'}));
+app.set('view engine', 'handlebars');
+
+// static files
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use(cookieParser());
+
+app.use(session({
+    secret: process.env.SECRETO,
+    key: process.env.KEY,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl : process.env.DATABASE })
+}))
+
+
+app.use('/', router());
+
+app.listen(process.env.PUERTO);
